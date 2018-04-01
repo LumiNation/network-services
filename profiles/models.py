e488f8afe5194f8e3d158d366bd65ff0c5ffbcb0
brian2795 <brianlee2795@gmail.com>
2018-04-01 14:27:43 -0400

    users and profiles

---
+++ profiles/models.py
@@ -0,0 +1,70 @@
+from django.db import models
+from django.db.models.signals import post_save
+from users.models import CustomUser as User
+from django.dispatch import receiver
+
+class DonorProfile(models.Model):
+	def __str__(self): 
+		return self.user.username
+
+	# Identification
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	birthdate = models.DateField(null=True, blank=True)
+	zipcode = models.CharField(blank=True, max_length=5)
+
+	# Giving Statistics
+	#donations: list[Donation]
+	#donation_tot: big-int			- evaluated
+	#social_score: big-int
+	#donor_dollars: big-int
+	#causes_reported: list[Cause] 
+	#causes_supported: list[Cause]	- evaluated
+	
+	# Project Info
+	#proj_following: list[Project]
+	#proj_supported: list[Project]	- evaluated
+	#proj_fav: list[Project]
+	#spon_supported: list[Sponsor]	- evaluated
+	#spon_fav: list[Sponsor]		
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+	if created:
+		DonorProfile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+	DonorProfile.objects.get(user=instance).save()
+
+
+"""
+class SponsorProfile(models.Model):
+	def __str__(self): 
+		return self.user.username
+
+	# Identification
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	birthdate = models.DateField(null=True, blank=True)
+	zipcode = models.CharField(blank=True, max_length=5)
+"""
+
+
+
+"""
+STUDENT = 1
+TEACHER = 2
+SUPERVISOR = 3
+ROLE_CHOICES = (
+	(STUDENT, 'Student'),
+	(TEACHER, 'Teacher'),
+	(SUPERVISOR, 'Supervisor'),
+)
+role = models.PositiveSmallIntegerField(choices=ROLE_CHOICES, null=True, blank=True)
+
+def was_published_recently(self):
+	now = timezone.now()
+	return now - datetime.timedelta(days=1) <= self.pub_date <= now
+was_published_recently.admin_order_field = 'pub_date'
+was_published_recently.short_description = 'Published recently?'
+"""
---
+++ users/admin.py
@@ -0,0 +1,36 @@
+from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
+from profiles.models import DonorProfile as Profile
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'Profile'
+    fk_name = 'user'
+
+class CustomUserAdmin(UserAdmin):
+    fieldsets = [
+        (None,              {'fields': ['username']}),
+        ('Personal Info',   {'fields': ['first_name', 'last_name', 'email', 'password'], 'classes': ['collapse']}),
+        ('Permissions',     {'fields': ['is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'], 'classes': ['collapse']}),
+        ('Important Dates', {'fields': ['date_joined', 'last_login'], 'classes': ['collapse']}),
+    ]
+
+    inlines = [ProfileInline, ]
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ['username', 'email', 'date_joined', 'is_active']
+    # list_filter = []
+    # search_fields = []
+
+    def get_inline_instances(self, request, obj=None):
+        if not obj:
+            return list()
+        return super(CustomUserAdmin, self).get_inline_instances(request, obj)
+
+admin.site.register(CustomUser, CustomUserAdmin)
