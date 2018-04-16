e774d1fcb55c794864e623c20422bb9411d9c9c4
Jung Lee <brianlee2795@gmail.com>
2018-04-16 00:56:48 -0400

    sponsors forms

--- sponsors/admin.py
+++ sponsors/admin.py
@@ -1,3 +1,57 @@
 from django.contrib import admin
+from organizations.models import Organization, OrganizationUser, OrganizationOwner
+from .forms import SponsorUserForm
+from .models import Sponsor, SponsorUser
+"""
+from .forms import SponsorForm
+class SponsorAdmin(admin.ModelAdmin):
+    model = Sponsor
+    form = SponsorForm
 
-# Register your models here.
+admin.site.register(Sponsor, SponsorAdmin)
+""" 
+
+class SponsorUserAdmin(admin.ModelAdmin):
+    model = SponsorUser
+    form = SponsorUserForm
+    readonly_fields = ('user',)
+
+admin.site.unregister(Organization)
+admin.site.unregister(OrganizationUser)
+admin.site.unregister(OrganizationOwner)
+admin.site.register(Sponsor)
+admin.site.register(SponsorUser, SponsorUserAdmin)
+
+
+
+"""
+class DonorInline(admin.StackedInline):
+    model = Donor
+    can_delete = False
+    verbose_name_plural = 'Donor Profile'
+    fk_name = 'user'
+
+
+class CustomUserAdmin(UserAdmin):
+    fieldsets = [
+        (None,              {'fields': ['username']}),
+        ('Personal Info',   {'fields': ['first_name', 'last_name', 'email', 'password'], 'classes': ['collapse']}),
+        ('Permissions',     {'fields': ['is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'], 'classes': ['collapse']}),
+        ('Important Dates', {'fields': ['date_joined', 'last_login'], 'classes': ['collapse']}),
+    ]
+
+    inlines = [DonorInline, ]
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ['username', 'email', 'date_joined', 'is_active']
+    # list_filter = []
+    # search_fields = []
+
+    def get_inline_instances(self, request, obj=None):
+        if not obj:
+            return list()
+        return super(CustomUserAdmin, self).get_inline_instances(request, obj)
+
+admin.site.register(CustomUser, CustomUserAdmin)
+"""
--- sponsors/models.py
+++ sponsors/models.py
@@ -1,3 +1,50 @@
+from django.core.validators import MinLengthValidator
 from django.db import models
+from organizations.models import Organization, OrganizationUser
 
-# Create your models here.
+# from sector_meta.utils import phone_regex
+
+
+class Sponsor(Organization):
+    ein 	= models.CharField(
+    	primary_key=True, 
+    	unique=True, 
+    	max_length=9, 
+    	validators=[MinLengthValidator(4),]
+    	)
+    # phone 	= models.CharField(validators=[phone_regex,], max_length=17, blank=True)
+    website = models.URLField(blank=True, max_length=20)
+    zipcode = models.CharField(blank=True, max_length=5)
+
+
+class SponsorUser(OrganizationUser):
+    class Meta:
+        proxy = True
+
+"""
+Sponsor
+	-Details-
+	Socia Media Links
+	Causes
+	
+	-Components-
+	Projects
+	Roles
+	SponsorUsers
+	
+
+SponsorUser
+	
+
+Roles
+	Permissions
+
+Permissions
+	Project Creation
+
+
+Default Roles
+	Executive Director
+	
+
+"""
